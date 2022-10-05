07ff0f56b2d895e29ba20d0f036524dad5d28c66
Cha-er-si <vpcharles31@gmail.com>
2022-10-05 11:22:08 +0800

    Updates

--- Portfolio Website/App_Start/BundleConfig.cs
+++ Portfolio Website/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Portfolio_Website
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-darkly.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
