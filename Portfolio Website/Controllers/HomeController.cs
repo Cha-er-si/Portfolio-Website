880f789c51d77fbc1c527e10986ba2c2adf3935a
Cha-er-si <vpcharles31@gmail.com>
2022-10-05 11:50:31 +0800

    work section responsiveness

--- Portfolio Website/Controllers/HomeController.cs
+++ Portfolio Website/Controllers/HomeController.cs
@@ -10,21 +10,7 @@ namespace Portfolio_Website.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
+            return View();        }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
